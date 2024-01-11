87479b01aa7b192096b80ff897be6772432abf68
xxnuo <bigtear@vip.qq.com>
2024-01-11 11:58:35 +0800

    暂时禁用log功能

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/getlantern/systray
+module github.com/xxnuo/systray
 
 go 1.13
 
--- systray_windows.go
+++ systray_windows.go
@@ -773,12 +773,12 @@ func (t *winTray) iconToBitmap(hIcon windows.Handle) (windows.Handle, error) {
 
 func registerSystray() {
 	if err := wt.initInstance(); err != nil {
-		log.Errorf("Unable to init instance: %v", err)
+		// log.Errorf("Unable to init instance: %v", err)
 		return
 	}
 
 	if err := wt.createMenu(); err != nil {
-		log.Errorf("Unable to create menu: %v", err)
+		// log.Errorf("Unable to create menu: %v", err)
 		return
 	}
 
@@ -796,7 +796,7 @@ func nativeLoop() {
 		Pt           point
 	}{}
 	for {
-		ret, _, err := pGetMessage.Call(uintptr(unsafe.Pointer(m)), 0, 0, 0)
+		ret, _, _ := pGetMessage.Call(uintptr(unsafe.Pointer(m)), 0, 0, 0)
 
 		// If the function retrieves a message other than WM_QUIT, the return value is nonzero.
 		// If the function retrieves the WM_QUIT message, the return value is zero.
@@ -804,7 +804,7 @@ func nativeLoop() {
 		// https://msdn.microsoft.com/en-us/library/windows/desktop/ms644936(v=vs.85).aspx
 		switch int32(ret) {
 		case -1:
-			log.Errorf("Error at message loop: %v", err)
+			// log.Errorf("Error at message loop: %v", err)
 			return
 		case 0:
 			return
@@ -845,11 +845,11 @@ func iconBytesToFilePath(iconBytes []byte) (string, error) {
 func SetIcon(iconBytes []byte) {
 	iconFilePath, err := iconBytesToFilePath(iconBytes)
 	if err != nil {
-		log.Errorf("Unable to write icon data to temp file: %v", err)
+		// log.Errorf("Unable to write icon data to temp file: %v", err)
 		return
 	}
 	if err := wt.setIcon(iconFilePath); err != nil {
-		log.Errorf("Unable to set icon: %v", err)
+		// log.Errorf("Unable to set icon: %v", err)
 		return
 	}
 }
@@ -879,19 +879,19 @@ func (item *MenuItem) parentId() uint32 {
 func (item *MenuItem) SetIcon(iconBytes []byte) {
 	iconFilePath, err := iconBytesToFilePath(iconBytes)
 	if err != nil {
-		log.Errorf("Unable to write icon data to temp file: %v", err)
+		// log.Errorf("Unable to write icon data to temp file: %v", err)
 		return
 	}
 
 	h, err := wt.loadIconFrom(iconFilePath)
 	if err != nil {
-		log.Errorf("Unable to load icon from temp file: %v", err)
+		// log.Errorf("Unable to load icon from temp file: %v", err)
 		return
 	}
 
 	h, err = wt.iconToBitmap(h)
 	if err != nil {
-		log.Errorf("Unable to convert icon to bitmap: %v", err)
+		// log.Errorf("Unable to convert icon to bitmap: %v", err)
 		return
 	}
 	wt.muMenuItemIcons.Lock()
@@ -900,7 +900,7 @@ func (item *MenuItem) SetIcon(iconBytes []byte) {
 
 	err = wt.addOrUpdateMenuItem(uint32(item.id), item.parentId(), item.title, item.disabled, item.checked)
 	if err != nil {
-		log.Errorf("Unable to addOrUpdateMenuItem: %v", err)
+		// log.Errorf("Unable to addOrUpdateMenuItem: %v", err)
 		return
 	}
 }
@@ -909,7 +909,7 @@ func (item *MenuItem) SetIcon(iconBytes []byte) {
 // only available on Mac and Windows.
 func SetTooltip(tooltip string) {
 	if err := wt.setTooltip(tooltip); err != nil {
-		log.Errorf("Unable to set tooltip: %v", err)
+		// log.Errorf("Unable to set tooltip: %v", err)
 		return
 	}
 }
@@ -922,7 +922,7 @@ func SetRemovalAllowed(allowed bool) {
 func addOrUpdateMenuItem(item *MenuItem) {
 	err := wt.addOrUpdateMenuItem(uint32(item.id), item.parentId(), item.title, item.disabled, item.checked)
 	if err != nil {
-		log.Errorf("Unable to addOrUpdateMenuItem: %v", err)
+		// log.Errorf("Unable to addOrUpdateMenuItem: %v", err)
 		return
 	}
 }
@@ -938,7 +938,7 @@ func (item *MenuItem) SetTemplateIcon(templateIconBytes []byte, regularIconBytes
 func addSeparator(id uint32) {
 	err := wt.addSeparatorMenuItem(id, 0)
 	if err != nil {
-		log.Errorf("Unable to addSeparator: %v", err)
+		// log.Errorf("Unable to addSeparator: %v", err)
 		return
 	}
 }
@@ -946,7 +946,7 @@ func addSeparator(id uint32) {
 func hideMenuItem(item *MenuItem) {
 	err := wt.hideMenuItem(uint32(item.id), item.parentId())
 	if err != nil {
-		log.Errorf("Unable to hideMenuItem: %v", err)
+		// log.Errorf("Unable to hideMenuItem: %v", err)
 		return
 	}
 }
